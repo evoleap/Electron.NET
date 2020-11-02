4f3977acafc157ccc30af299af9496f6f1466805
Prasanna Parthasarathy <p.parthasarathy@evoleap.com>
2020-11-02 14:07:21 -0600

    Switched to ES5 module format

--- ElectronNET.Host/wix-install-builder.mjs
+++ ElectronNET.Host/wix-install-builder.mjs
@@ -1,10 +1,14 @@
-import { MSICreator } from 'electron-wix-msi';
+var ewix = require('electron-wix-msi');
 const msiCreatorConf = require('./wix-config')
 // Step 1: Instantiate the MSICreator
-const msiCreator = new MSICreator(msiCreatorConf);
+
+const msiCreator = new ewix.MSICreator(msiCreatorConf);
 
 // Step 2: Create a .wxs template file
-const supportBinaries = await msiCreator.create();
+const supportBinaries = msiCreator.create().then(function() {
+    // Step 3: Compile the template to a .msi file
+    msiCreator.compile();
+});
 
 // // 🆕 Step 2a: optionally sign support binaries if you
 // // sign you binaries as part of of your packaging script
@@ -14,5 +18,3 @@ const supportBinaries = await msiCreator.create();
 //   await signFile(binary);
 // });
 
-// Step 3: Compile the template to a .msi file
-await msiCreator.compile();
